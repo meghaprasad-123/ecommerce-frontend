2b2d3e8220b9aca7d000b5d2c1a9c473faf151cd
megha prasad <meghaprasadkeecheril@gmail.com>
2023-01-10 10:01:43 +0530

    front initial

---
+++ src/app/products/all-products/all-products.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-all-products',
+  templateUrl: './all-products.component.html',
+  styleUrls: ['./all-products.component.css']
+})
+export class AllProductsComponent implements OnInit {
+  allproducts:any=[];   //all products array of details
+
+  searchterm:string='';
+
+  constructor(private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getProducts().subscribe(
+      (data:any)=>{
+        this.allproducts=data.products
+      }
+    )
+
+    this.api.searchkey.subscribe(
+      (data:any)=>{
+        this.searchterm=data
+      }
+    )
+  }
+
+
+  addtowishlist(product:any){
+    this.api.addtowishlist(product).subscribe(
+      (result:any)=>{
+        //server to client
+        alert(result.message) //added successfully
+      },
+      (result:any)=>{
+        alert(result.error.message)  //error
+      }
+    )
+  }
+}
---
+++ src/app/products/api.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  //to share stream of data
+  searchkey=new BehaviorSubject('')
+
+  constructor(private http:HttpClient) { }
+
+  getProducts(){
+    return this.http.get('http://localhost:3000/all-products')
+  }
+
+  //add to wishlist
+  addtowishlist(product:any){
+    const body={
+      id:product.id,
+      title:product.title,
+      price:product.price,
+      image:product.image,
+      description:product.description
+    }
+    return this.http.post('http://localhost:3000/addtowishlist',body)
+  }
+}
